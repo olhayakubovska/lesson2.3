076638a3af960da9e9f70276c7f324b7eb15b33f
ola <oyakubovska80@gmail.com>
2024-09-13 00:39:08 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const NUMS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+export const App = () => {
+  const [operand1, setOperand1] = useState("");
+  const [operator, setOperator] = useState("");
+  const [operand2, setOperand2] = useState("");
+  const [res, setRes] = useState(null);
+
+  const reset = () => {
+    setRes(null);
+    setOperand1("");
+    setOperator("");
+    setOperand2("");
+  };
+
+  const equals = () => {
+    let num;
+    if (operator === "+") {
+      num = Number(operand1) + Number(operand2);
+    }
+
+    if (operator === "-") {
+      num = Number(operand1) - Number(operand2);
+    }
+    setRes(num);
+  };
+
+  const fn1 = (num) =>
+    operator === "" ? setOperand1(operand1 + num) : setOperand2(operand2 + num);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="window">
+        {res ? (
+          res
+        ) : (
+          <>
+            {" "}
+            {operand1}
+            {operator}
+            {operand2}
+          </>
+        )}
+      </div>
+      <div className="abc">
+        <div className="numbers">
+          {NUMS.map((num, index) => (
+            <div className="cell" key={index} onClick={() => fn1(num)}>
+              {num}
+            </div>
+          ))}
+        </div>
+        <div className="operators">
+          <div className="cell" onClick={() => setOperator("+")}>
+            +
+          </div>
+          <div className="cell" onClick={() => setOperator("-")}>
+            -
+          </div>
+          <div className="cell" onClick={equals}>
+            =
+          </div>
+          <div className="cell" onClick={reset}>
+            C
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
